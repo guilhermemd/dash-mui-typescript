8b004eb4dbbbb288392a507e0304103dfbe25eff
guilherme <guilherme.md@gmail.com>
2022-05-30 21:59:20 -0300

    feat - add theme light with MUI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,14 @@ import { Button } from "@mui/material";
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button>Super Botão</Button>} />
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary">
+            Super Botão
+          </Button>
+        }
+      />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );

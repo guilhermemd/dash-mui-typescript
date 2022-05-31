f0534ab11422e6992fb7f3d7f24ed268eb8ba97b
guilherme <guilherme.md@gmail.com>
2022-05-30 21:35:16 -0300

    feat - install MUI and import on routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export {};
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Button } from "@mui/material";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<Button>Super Botão</Button>} />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};

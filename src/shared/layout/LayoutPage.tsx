4fb8a4c24257d5317a8cd8b5bdbb2252cbf6c751
guilherme <guilherme.md@gmail.com>
2022-06-06 20:35:16 -0300

    feat - lesson 11 - menu bar on mobile with click

--- src/shared/layout/LayoutPage.tsx
+++ src/shared/layout/LayoutPage.tsx
@@ -1,13 +1,39 @@
-import { Box } from '@mui/system';
+import {
+  Box,
+  Typography,
+  useTheme,
+  IconButton,
+  Icon,
+  useMediaQuery
+} from '@mui/material';
+
+import { useDrawerContext } from '../contexts';
 
 interface ILayoutPage {
   titulo: string;
   children?: React.ReactNode;
 }
 export const LayoutPage: React.FC<ILayoutPage> = ({ children, titulo }) => {
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const { toggleDrawerOpen } = useDrawerContext();
   return (
     <Box height="100%" display="flex" flexDirection="column" gap={1}>
-      <Box>{titulo}</Box>
+      <Box
+        padding={1}
+        height={theme.spacing(12)}
+        display="flex"
+        alignItems="center"
+        gap={1}
+      >
+        {smDown && (
+          <IconButton onClick={toggleDrawerOpen}>
+            <Icon>menu</Icon>
+          </IconButton>
+        )}
+        <Typography variant="h5">{titulo}</Typography>
+      </Box>
 
       <Box>Barra de Ferramentas</Box>
 
